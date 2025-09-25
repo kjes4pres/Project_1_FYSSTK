545b657e465bca71ec77122a7434563bc3489052
kjes4pres <stangelandkjersti@gmail.com>
2025-09-25 10:18:34 +0200

    update imports

--- Code/functions.py
+++ Code/functions.py
@@ -11,6 +11,17 @@ September 2025
 '''
 
 import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+from sklearn.utils import resample
+from sklearn.pipeline import make_pipeline
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import Ridge
+from sklearn.metrics import mean_squared_error, r2_score
+from sklearn.linear_model import Ridge
 
 np.random.seed(2018)
 
